2bcdc8a91199bb0bd39b3f4b039815ff8021d6e4
G <69143512+N3gev@users.noreply.github.com>
2022-10-19 14:48:52 +0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,17 +1,24 @@
 import discord
 
-class MyClient(discord.Client):
-    async def on_ready(self):
-        print(f'Logged on as {self.user}!')
-
-    async def on_message(self, message):
-        print(f'Message from {message.author}: {message.content}')
-
 intents = discord.Intents.default()
 intents.message_content = True
 
-client = MyClient(intents=intents)
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
 
 tokenFail = open("token.txt","r", encoding="UTF-8")
+token = tokenFail.read()
+client.run(token)
 tokenFail.close()
-client.run()
+
